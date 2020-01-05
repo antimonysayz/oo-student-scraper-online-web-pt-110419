f9fcdd9768d0d0429878e0589e8eb339759fc3a8
Anthony Mai <anthony.mai530@gmail.com>
2020-01-05 22:26:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,17 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
+    student_hash = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css(".student-card").collect{|student|
+      hash = {
+        name: student.css('h4.student-name').text
+        location: student.css('p.student-location')
+        profile_url: student.css('a').attribute('href')
+      }
+      student_hash << hash
+    }
+    students_hash
   end
 
   def self.scrape_profile_page(profile_url)
